846be4f46875ae0aaf5c07ac34d634d61994dbaa
Gustavo Silva <guh600@gmail.com>
2021-11-22 22:33:43 -0300

    Add sections testimonials and contact

--- main.js
+++ main.js
@@ -21,3 +21,28 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scroll')
     }
 })
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+`, { interval: 100}
+)
