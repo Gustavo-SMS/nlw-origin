e1d8f0cc7852912c0259cf3b41d6bca81677885f
Gustavo Silva <guh600@gmail.com>
2021-11-23 22:21:41 -0300

    Added footer

--- main.js
+++ main.js
@@ -11,16 +11,16 @@ for (const link of links) {
     link.addEventListener('click', () => nav.classList.remove('show'))
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', () => {
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
 
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
@@ -43,6 +43,22 @@ scrollReveal.reveal(`
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
+    #contact .text, #contact .links,
+    footer .brand, footer .social
 `, { interval: 100}
 )
+
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+    
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+window.addEventListener('scroll', () => {
+    changeHeaderWhenScroll()
+    backToTop()
+})
